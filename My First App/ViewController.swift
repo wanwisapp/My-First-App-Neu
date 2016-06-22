3af3d219d902f61039299ced3b9651bffd8cca66
James <Enkanpong1308@gmail.com>
2016-06-22 10:17:08 +0700

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by iMac_16 on 6/22/2559 BE.
+//  Copyright © 2559 Wanwisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
